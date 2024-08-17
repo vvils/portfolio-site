6b007ce693f1bd7f32fbf5c0f83800bd163511c9
wils <92552628+vvils@users.noreply.github.com>
2024-08-16 23:17:08 -0400

    deploy test

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "images.pexels.com" }],
+    loader: "custom",
+    loaderFile: "./loader.js",
   },
+  output: "export",
+  basePath: "/Portfolio-Site",
 };
 
 export default nextConfig;
